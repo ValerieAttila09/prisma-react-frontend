cde49204daafa79112deab9c204237f5a53d226b
ValerieAttila09 <akunvalerie76@gmail.com>
2025-08-10 00:34:42 +0700

    committed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Navbar() {
+  return (
+    <div className="max-w-2xl mx-auto px-4 py-2 bg-transparent flex items-center justify-between">
+      <div className="flex items-center gap-2"></div>
+      <div className="flex items-center gap-2"></div>
+    </div>
+  )
+}
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,8 @@
+import LandingPageComponent from "../components/LandingPage";
+
+
+export default function LandingPage() {
+  return (
+    <LandingPageComponent/>
+  )
+}
